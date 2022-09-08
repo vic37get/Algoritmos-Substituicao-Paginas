6a2740c6dace8851ff2f815acd62b7d12e345b4d
Victor <victorribeiro172@gmail.com>
2022-09-08 17:33:37 -0300

    Algoritmo Ótimo #2 implementação base do algoritmo

--- algoritmoOtimo.py
+++ algoritmoOtimo.py
@@ -19,4 +19,38 @@ def TratamentoArquivo(arquivo):
     return arquivo
 
 def AlgoritmoOtimo(dados):
-    return
+    dados = TratamentoArquivo(dados)
+    falta_paginas = 0
+    qntd_molduras = dados[0]
+    referencias = dados[1:]
+    paginas = []
+
+    for indice, referencia in enumerate(referencias):
+        if referencia not in paginas:
+            falta_paginas+=1
+        if len(paginas) == qntd_molduras:
+            pagMaiorRotulo = PaginaComMaiorRotulo(indice, paginas, referencias)
+            print(referencia, pagMaiorRotulo)
+            paginas.remove(pagMaiorRotulo)
+        
+        paginas.append(referencia)
+    
+    return falta_paginas
+
+
+def PaginaComMaiorRotulo(indicepag, paginas, referencias):
+    tempos = [-1 for i in range(len(paginas))]
+    for indice, pagina in enumerate(paginas):
+        for referencia in range(indicepag+1, len(referencias)):
+            if referencias[referencia] != pagina:
+                tempos[indice]+=1
+            else:
+                break
+    maiorTempo = np.argmax(tempos)
+    pagMaisDemorada = paginas[maiorTempo]
+    
+    return pagMaisDemorada
+
+dados = OpenFile()
+falta_pagina = AlgoritmoOtimo(dados)
+print('OTM %d' %(falta_pagina))
