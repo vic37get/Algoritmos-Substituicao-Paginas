f57baa4b55091d51f807fb144b3fd32e05f487fb
Victor <victorribeiro172@gmail.com>
2022-09-10 09:59:46 -0300

    #2 Finalização do algoritmo ótimo

--- algoritmoOtimo.py
+++ algoritmoOtimo.py
@@ -18,26 +18,6 @@ def TratamentoArquivo(arquivo):
         arquivo[linha] = int(arquivo[linha])
     return arquivo
 
-def AlgoritmoOtimo(dados):
-    dados = TratamentoArquivo(dados)
-    falta_paginas = 0
-    qntd_molduras = dados[0]
-    referencias = dados[1:]
-    paginas = []
-
-    for indice, referencia in enumerate(referencias):
-        if referencia not in paginas:
-            falta_paginas+=1
-        if len(paginas) == qntd_molduras:
-            pagMaiorRotulo = PaginaComMaiorRotulo(indice, paginas, referencias)
-            print(referencia, pagMaiorRotulo)
-            paginas.remove(pagMaiorRotulo)
-        
-        paginas.append(referencia)
-    
-    return falta_paginas
-
-
 def PaginaComMaiorRotulo(indicepag, paginas, referencias):
     tempos = [-1 for i in range(len(paginas))]
     for indice, pagina in enumerate(paginas):
@@ -50,6 +30,34 @@ def PaginaComMaiorRotulo(indicepag, paginas, referencias):
     pagMaisDemorada = paginas[maiorTempo]
     
     return pagMaisDemorada
+
+def AlgoritmoOtimo(dados):
+    dados = TratamentoArquivo(dados)
+    falta_paginas = 0
+    qntd_molduras = dados[0]
+    referencias = dados[1:]
+    paginas = []
+
+    for indice, referencia in enumerate(referencias):
+        print('A referencia {} chegou'.format(referencia))
+        if referencia not in paginas:
+            print('A referencia {} não está na moldura: {}'.format(referencia, paginas))
+            falta_paginas+=1
+            print('Falta de páginas: {}'.format(falta_paginas))
+            if len(paginas) == qntd_molduras:
+                print('O número de páginas é igual a quantidade de molduras!!')
+                print('Moldura: {}'.format(paginas))
+                pagMaiorRotulo = PaginaComMaiorRotulo(indice, paginas, referencias)
+                print('A pagina com maior rótulo é: {}'.format(pagMaiorRotulo))
+                paginas.remove(pagMaiorRotulo)
+                print('A pagina com maior rotulo foi removida! Moldura: {}'.format(paginas))
+            paginas.append(referencia)
+            print('A referencia {} foi adicionada!'.format(referencia))
+            print('Moldura após adicionar a referencia {}: {}'.format(referencia,paginas))
+        else:
+            print('A referencia {} já estava na moldura!'.format(referencia))
+        print('\n')
+    return falta_paginas
 
 dados = OpenFile()
 falta_pagina = AlgoritmoOtimo(dados)
